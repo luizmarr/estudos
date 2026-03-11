467b34027adf7beab3f681fee5544d0c4212c9f7
luizmarr <luizhenriquemarreira08@gmail.com>
2026-03-11 11:29:56 -0300

    adicionando uma opção pra escolher o mês do feriado

---
+++ api_holidays/ex_03.py
@@ -0,0 +1,65 @@
+import json
+import requests
+
+
+
+def get_input():
+    year = input("Digite o ano para consultar os feriados: ")
+    country_code = input("Digite o código do país (ex: BR para Brasil): ")
+    month = int(input("Digite o mês para consultar os feriados (opcional, deixe em branco para todos os meses): ") or 0)
+    return year, country_code, month
+
+
+def get_holidays(year: str, country_code: str, month: int):
+    """
+    Consulta os feriados públicos de um país para um determinado ano usando a API do Nager.Date.
+    Args:
+        year (str): O ano para o qual os feriados devem ser consultados.
+        country_code (str): O código do país (ex: BR para Brasil).
+        month (str): O mês para o qual os feriados devem ser consultados (opcional).
+    Returns:
+        list: Uma lista de dicionários contendo informações sobre os feriados, incluindo a data,
+              o nome local e o nome em inglês.
+    """
+    url = f"https://date.nager.at/api/v3/PublicHolidays/{year}/{country_code}"
+    response = requests.get(url=url, verify=False)
+    data = json.loads(response.content.decode("utf-8"))
+    return data
+
+def extrair_dados_feriado(holiday: dict):
+    '''Extrai a data, o nome local e o nome em inglês de um dicionário de feriado.'''
+    date = holiday['date']
+    local_name = holiday['localName']
+    name = holiday['name']
+    month = date.split("-")[1]  # Extrai o mês da data (formato YYYY-MM-DD)
+    return date, local_name, name
+
+def print_country(year: str, country_code: str, month: int):
+    '''Imprime o nome do país e o ano para os quais os feriados estão sendo consultados.'''
+    if month:
+        print(f"\nFeriados em {country_code} para o ano de {year} e o mês de {month}:")
+    else:
+        print(f"\nFeriados em {country_code} para o ano de {year}:")
+
+def print_holiday(date: str, local_name: str, name: str):  
+    '''imprime as infirmaçoes do feriado'''
+    print(f"{date}: {local_name} ({name})")
+
+
+def main(year: str, country_code: str, month: str):
+    data = get_holidays(year, country_code, month)
+
+    print_country(year, country_code, month)
+
+    for holiday in data:
+        print(f"{holiday['date']}: {holiday['localName']} ({holiday['name']})")
+    date, local_name, name = extrair_dados_feriado(holiday)
+    print_holiday(date, local_name, name)  
+
+if __name__ == "__main__":
+    # year, country_code, month = get_input()
+    year = "2026"
+    country_code_list = ["BR", "US", "DE", "FR", "IT"]
+    month = 12  # Exemplo: consultar feriados do mês de dezembro
+    for country_code in country_code_list:
+        main(year, country_code, month)
