253f031e06bf25e256f48acae6444f773c4b7ada
luizmarr <luizhenriquemarreira08@gmail.com>
2026-04-09 12:22:29 -0300

    colocando a opçao do usuario colocsr o ano com apenas 2 digitos

--- calc_age/main.py
+++ calc_age/main.py
@@ -9,8 +9,17 @@ class Person:
             name (str): O nome da pessoa.
             birth_date (str): A data de nascimento da pessoa no formato "YYYY-MM-DD".
         """
+        formatos_validos = ["%Y-%m-%d", "%y-%m-%d"]
         self.name = name
-        self.birth_date = datetime.strptime(birth_date, "%Y-%m-%d")
+        self.birth_date = None
+
+        for formato in formatos_validos:
+            try:
+                self.birth_date = datetime.strptime(birth_date, formato)
+                break
+            except ValueError:
+                continue
+        raise ValueError("Data de nascimento inválida.")
 
     def calculate_age(self) -> int:
         today = datetime.today()
@@ -20,9 +29,9 @@ class Person:
         if (today.month, today.day) < (self.birth_date.month, self.birth_date.day):
             age -= 1
 
-        print(f"{person.name} tem {age} anos.")
+        print(f"{self.name} tem {age} anos.")
 
-    def validate_date(self, date_str: str) -> bool:
+    def validate_date(date_str: str) -> bool:
         """Valida se a data fornecida está no formato correto e é uma data válida.
         Args:
             date_str (str): A data a ser validada no formato "YYYY-MM-DD".
@@ -35,14 +44,20 @@ class Person:
         except ValueError:
             return False
 
-    # LUIZ precisa criar um metodo para validar se a data e correta
+  
     # aceitar ano com dois digitos e com quatro digitos
 
 if __name__ == "__main__":
     name = input("Digite o nome da pessoa: ")
-    birth_date = input("Digite a data de nascimento (YYYY-MM-DD): ")
 
-    person = Person(name, birth_date)
-    age = person.calculate_age()
+    while True:
+        birth_date = input("Digite a data de nascimento (YYYY-MM-DD ou YY-MM-DD): ")
+        if Person.validate_date(birth_date):
+            break
+        else:
+            print("Data de nascimento inválida. Por favor, tente novamente.")
+    birth_date = input("Digite a data de nascimento: ")
+
+    
 
     
