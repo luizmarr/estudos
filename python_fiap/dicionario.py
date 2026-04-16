3dfb12e22c9dd47eca3eca71bd6e293fb1bb2329
luizmarr <luizhenriquemarreira08@gmail.com>
2026-04-16 12:35:59 -0300

    adicionando um exercicio de dicionario que serve para cadastrar usuarios

---
+++ python_fiap/dicionario.py
@@ -0,0 +1,38 @@
+usuarios = {}
+def cadastrar_usuario(nome, email):
+    if email in usuarios:
+        print("Usuário já cadastrado.")
+    else:
+        usuarios[email] = nome
+        print("Usuário cadastrado com sucesso.")
+
+def exibir_usuarios():
+    if usuarios:
+        print("Usuários cadastrados:")
+        for email, nome in usuarios.items():
+            print(f"Nome: {nome}, Email: {email}")
+    else:
+        print("Nenhum usuário cadastrado.")
+
+def main():
+    while True:
+        print("\nMenu:")
+        print("1. Cadastrar usuário")
+        print("2. Exibir usuários")
+        print("3. Sair")
+        escolha = input("Escolha uma opção: ")
+
+        if escolha == "1":
+            nome = input("Digite o nome do usuário: ")
+            email = input("Digite o email do usuário: ")
+            cadastrar_usuario(nome, email)
+        elif escolha == "2":
+            exibir_usuarios()
+        elif escolha == "3":
+            print("Saindo do programa.")
+            break
+        else:
+            print("Opção inválida. Por favor, tente novamente.")
+
+if __name__ == "__main__":
+    main()
