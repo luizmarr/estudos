600fec907e48b761d904c4731f962569b3bce7f9
luizmarr <luizhenriquemarreira08@gmail.com>
2026-04-22 10:12:53 -0300

    adicionando uma função de remover o usuario

--- python_fiap/dicionario.py
+++ python_fiap/dicionario.py
@@ -14,12 +14,20 @@ def exibir_usuarios():
     else:
         print("Nenhum usuário cadastrado.")
 
+def remover_usuario(email):
+    if email in usuarios:
+        del usuarios[email]
+        print("Usuário removido com sucesso.")
+    else:
+        print("Usuário não encontrado.")
+
 def main():
     while True:
         print("\nMenu:")
         print("1. Cadastrar usuário")
         print("2. Exibir usuários")
-        print("3. Sair")
+        print("3. Remover usuário")
+        print("4. Sair")
         escolha = input("Escolha uma opção: ")
 
         if escolha == "1":
@@ -29,6 +37,9 @@ def main():
         elif escolha == "2":
             exibir_usuarios()
         elif escolha == "3":
+            email = input("Digite o email do usuário a ser removido: ")
+            remover_usuario(email)
+        elif escolha == "4":
             print("Saindo do programa.")
             break
         else:
