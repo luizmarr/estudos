199ea6afafa80a74d94ddf5762a59336ba3d859c
luizmarr <luizhenriquemarreira08@gmail.com>
2026-04-24 11:45:56 -0300

    excluindo o arquivo iris.data e separando meu codigo em funçoes para manipulação de arquivos

--- python_fiap/manipulaçao2.py
+++ python_fiap/manipulaçao2.py
@@ -1,8 +1,30 @@
-database = []
-with open("iris.data", "r") as arquivo:
-    for registro in arquivo:
-        registro = registro.strip()
-        if registro:
-            database.append(registro.split(","))
+def ler_arquivo(nome_arquivo):
+    with open(nome_arquivo, 'r') as arquivo:
+        conteudo = arquivo.read()
+    return conteudo
 
-print(database)
+def editar_arquivo(nome_arquivo, novo_conteudo):
+    with open(nome_arquivo, 'w') as arquivo:
+        arquivo.write(novo_conteudo)
+
+def main():
+    print("Bem-vindo ao manipulador de arquivos!")
+    while True:
+        opção = input("Digite a opção desejada (1 - Ler arquivo, 2 - Editar arquivo, 3 - Sair): ")
+        if opção == "1":
+            nome_arquivo = input("Digite o nome do arquivo para ler: ")
+            conteudo = ler_arquivo(nome_arquivo)
+            print("Conteúdo do arquivo:")
+            print(conteudo)
+        elif opção == "2":
+            nome_arquivo = input("Digite o nome do arquivo para editar: ")
+            novo_conteudo = input("Digite o novo conteúdo para o arquivo: ")
+            editar_arquivo(nome_arquivo, novo_conteudo)
+            print("Arquivo editado com sucesso.")
+        elif opção == "3":
+            print("Saindo...")
+            break
+        else:
+            print("Opção inválida.")
+      
+if __name__ == "__main__":    main()
